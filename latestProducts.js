0055b646ab41ea9cce8d071bacacb7f824568673
PacoRaton <pacocasino1@hotmail.com>
2019-01-17 17:00:50 -0600

    Pagina web de prueba de Francisco Sandoval

---
+++ latestProducts.js
@@ -0,0 +1,62 @@
+{
+    "products": [
+        {
+            "id": 234,
+            "name": "Protein delicious",
+            "price": 2300,
+            "specialPrice": 1980,
+            "inSale": true,
+            "isNew": false    
+        },
+        {
+            "id": 237,
+            "name": "Special burger",
+            "price": 2000,
+            "specialPrice": null,
+            "inSale": false,
+            "isNew": true  
+        },
+        {
+            "id": 236,
+            "name": "Secret recipes",
+            "price": 500,
+            "specialPrice": 320,
+            "inSale": true,
+            "isNew": false  
+        },
+        {
+            "id": 235,
+            "name": "Vegan Style",
+            "price": 1100,
+            "specialPrice": null,
+            "inSale": false,
+            "isNew": false    
+        }
+    ],
+    "reseñas": [
+        {
+            "id": 234,
+            "stars": 5
+        },
+        {
+            "id": 234,
+            "stars": 2
+        },
+        {
+            "id": 234,
+            "stars": 2
+        },
+        {
+            "id": 236,
+            "stars": 4
+        },
+        {
+            "id": 236,
+            "stars": 2
+        },
+        {
+            "id": 237,
+            "stars": 5
+        }
+    ]
+}
